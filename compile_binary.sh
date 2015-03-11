b3468bdeb7475ab7981320c9477fbda2bcfeb6dd
Alistair Thomas <astavale@yahoo.co.uk>
2015-03-11 13:09:52 +0000

    Added file structure for Device classes

--- compile_binary.sh
+++ compile_binary.sh
@@ -3,4 +3,8 @@
 /home/al/software_projects/vala_source/installed/bin/valac \
 	src/init.gs \
 	src/CLI_Options.gs \
+	src/devices/DeviceFactory.gs \
+	src/devices/NoDevice.gs \
+	src/devices/BlockDevice.gs \
+	src/devices/FileAsDevice.gs \
 	--output build_os_image
