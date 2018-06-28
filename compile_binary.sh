f8337de5a338913ee25e33fef0aafb67d6ab1979
Alistair Thomas <astavale@yahoo.co.uk>
2018-06-28 14:52:01 +0100

    Add augeas.vapi

--- compile_binary.sh
+++ compile_binary.sh
@@ -44,7 +44,7 @@ $VALAC \
 	src/PackageManagers/PackageManagerFactory.gs \
 	src/PackageManagers/RPMPackageManager.gs \
 	build/src/resources/resources.c \
-	--vapidir /home/al/software_projects/vapis/ \
+	--vapidir ./vapi/ \
 	--pkg gio-2.0 \
 	--pkg posix \
 	--pkg linux \
